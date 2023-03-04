3183b2a534eed592176a148959fda3662d2ba6cd
kevinminnn <min123@uw.edu>
2023-03-03 21:09:10 -0800

    MP0 and MP1 Pages: Finished

---
+++ mp1.js
@@ -0,0 +1,16 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlides() {
+  slides.forEach(slide => slide.classList.remove('active'));
+  slides[slideIndex].classList.add('active');
+  slideIndex++;
+
+  if (slideIndex === slides.length) {
+    slideIndex = 0;
+  }
+
+  setTimeout(showSlides, 3000);
+}
+
+showSlides();
