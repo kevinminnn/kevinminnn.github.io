be4e0f2777248045b8438350f1a0cab2fd6d347a
kevinminnn <min123@uw.edu>
2023-07-18 00:22:47 -0700

    Updated Title Covers + Sticky Sections

---
+++ project_home.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    var threshold = 200; // Adjust this value as needed
+    
+    $(window).scroll(function() {
+        var scrollHeight = $(window).scrollTop();
+
+        $('#content-wrap .image-container').each(function() {
+            var itemHeight = $(this).outerHeight();
+            var itemOffset = $(this).offset().top;
+
+            if (scrollHeight > itemOffset - threshold && scrollHeight < (itemOffset + itemHeight)) {
+                $(this).addClass('sticky');
+            } else {
+                $(this).removeClass('sticky');
+            }
+        });
+    });
+});
+
